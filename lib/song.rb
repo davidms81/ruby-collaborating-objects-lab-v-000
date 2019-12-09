6ac6439c3eb13210977a6d61130d2c05d53de845
David Schoenholtz <dmschoenholtz@gmail.com>
2019-12-09 00:41:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,8 @@
 class Song
   attr_accessor :name, :artist
-  @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def self.new_by_filename(filename)
@@ -22,5 +16,4 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-
 end
